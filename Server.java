a1287dda1f080434ceda20ddb2dfc883b4687049
Mogetti-Matteo <mmatteo0105@gmail.com>
2023-02-20 13:38:53 +0100

    server funz

--- Server.java
+++ Server.java
@@ -1,5 +1,7 @@
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.io.IOException;
 import java.net.DatagramPacket;
 
 public class Server {
@@ -10,7 +12,20 @@ public class Server {
         byte [] sendData = new byte[1024];
 
         while(true){
+            try{
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            server.receive(receivePacket);
+            String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizaedMessage = message.toUpperCase();
+            sendData = capitalizaedMessage.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            server.send(sendPacket);
+            }
+            catch(IOException e){
+                System.err.println(e);
+            }
         }
         }
         catch(SocketException e){
