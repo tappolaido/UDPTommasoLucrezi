a51d1c5f959398c843648fb2d352c019fd989662
tappolaido <tommaso.lucrezi7@gmail.com>
2023-02-27 12:16:12 +0100

    commit tommaso

--- Client.java
+++ Client.java
@@ -2,5 +2,6 @@ public class Client{
     public static void main(String[] args){
 
         
+        
     }
 }
--- Server.java
+++ Server.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 
 
+
 public class Server {
     public static void main(String[] args){
         try{
